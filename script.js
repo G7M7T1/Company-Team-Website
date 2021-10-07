cdc375af9ee0f0ae257de877c56b2e34f7331d5f
G7M7T1 <genlisense@gmail.com>
2021-10-07 04:13:38 -0400

    Add Some JS
    Scrollreveal.js and WickedCSS

---
+++ script.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal(".section-1-heading", { delay: 200, duration: 3000 });
+ScrollReveal().reveal(".section-heading", { delay: 100, duration: 2000 });
+ScrollReveal().reveal(".customer.card-3", { delay: 200, duration: 1000 });
+ScrollReveal().reveal(".customer.card-2", { delay: 200, duration: 1000 });
+ScrollReveal().reveal(".customer.card-1", { delay: 200, duration: 1000 });
